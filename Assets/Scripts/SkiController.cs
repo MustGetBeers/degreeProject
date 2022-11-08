b6b9e14e111d504b6c0d565ab827e31637a8d21d
Domhnall Henderson <dihenderson@hotmail.co.uk>
2022-11-08 16:52:41 +0100

    the ragdoll can dance now
    also a postprocessing volume for some reason

--- Assets/Scripts/SkiController.cs
+++ Assets/Scripts/SkiController.cs
@@ -7,20 +7,29 @@ public class SkiController : MonoBehaviour
 {
 
     private Vector2 movementDirection;
-    public float speed;
+    private Vector2 lookDirection;
+
     public Rigidbody rb;
+    public ConfigurableJoint hipJoint;
+    public Transform head;
+
+    public float speed;
     public float addForceValue;
 
     // Update is called once per frame
     void Update()
     {
         Move();
+
+        //Commented this out for now whilst I deal with moving a bit
+        //Look();
     }
 
 
     public void OnFire()
     {
-        rb.AddForce(Vector3.forward * addForceValue, ForceMode.Force);
+        rb.AddForce(Vector3.up * addForceValue, ForceMode.Force);
+    
     }
 
     public void OnMove(InputValue value)
@@ -28,6 +37,11 @@ public class SkiController : MonoBehaviour
         // Sets the movement direction from the value of the input value WASD
         movementDirection = value.Get<Vector2>();
 
+    }
+
+    public void OnLook(InputValue value)
+    {
+        lookDirection = value.Get<Vector2>();
     }
 
     public void Move()
@@ -39,5 +53,27 @@ public class SkiController : MonoBehaviour
         rb.AddForce(movementDirection.y * addForceValue * Time.deltaTime * Vector3.forward);
         rb.AddForce(movementDirection.x * addForceValue * Time.deltaTime * Vector3.right);
 
+        Vector3 direction = new Vector3(movementDirection.x, 0f, movementDirection.y).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+
+            //Not really sure what all this doing, I should get it explained... the targetAngle is deffo wrong here.
+            this.hipJoint.targetRotation = Quaternion.Euler(0f, targetAngle, 0f);
+
+            this.rb.AddForce(direction * speed * Time.fixedDeltaTime);
+
+        }
+    }
+
+    public void Look()
+    {
+
+        Vector3 direction = new Vector3(lookDirection.x, 0f, lookDirection.y).normalized;
+
+        float targetAngle = Mathf.Atan2(direction.z, direction.x) * Mathf.Rad2Deg;
+
+        head.rotation = Quaternion.Euler(0f, targetAngle, 0f);
     }
 }
