0e2ca7c005f702c00557f929fec7c9b2ab8bf38c
Domhnall Henderson <dihenderson@hotmail.co.uk>
2022-12-14 10:54:42 +0100

    added finnish line and timer

--- Assets/CheckpointManager.cs
+++ Assets/CheckpointManager.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CheckpointManager : MonoBehaviour
 {
 
-    public GameObject[] checkpointsArray;
+    public List<GameObject> checkpointsList;
+    public int missedCheckpoints;
+    public float timer;
+    private bool hasFinished;
+    public TextMeshProUGUI timerText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +21,24 @@ public class CheckpointManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (!hasFinished)
+        {
+            timer += Time.deltaTime;
+            timerText.text = timer.ToString("n2");
+        }
+    }
+
+    public void CheckpointPast(GameObject checkpoint)
+    {
+        if (checkpointsList.Contains(checkpoint))
+        {
+            checkpointsList.Remove(checkpoint);
+        }
         
+    }
+    public void FinishLinePast()
+    {
+        missedCheckpoints = checkpointsList.Count;
+        hasFinished = true;
     }
 }
